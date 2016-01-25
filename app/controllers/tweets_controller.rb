5d22f247653e22ed35ffe981f68a1214b30fe2f6
Philip Greenwald <philip@avidest.com>
2016-01-25 18:25:44 -0500

    Learned about params hash and strong params

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,15 @@ class TweetsController < ApplicationController
 		@tweet = Tweet.new
 	end
 
+	def create
+		#when we used params[:tweet], it was equivalent to @tweet = Tweet.new(content: 'whatever your tweet is')
+		
+		@tweet = Tweet.new(tweet_params)
+		puts "AN ERROR MESSAGE: #{@tweet.inspect}" #puts outputs your message to the server logs (in terminal)
+	end
+
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
 end
