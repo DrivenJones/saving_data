1474685173c90cfe2008da705c3917b2801a5f11
Philip Greenwald <philip@avidest.com>
2016-01-25 17:36:45 -0500

    Created a new tweet view and controller

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,11 @@
 class Tweet < ActiveRecord::Base
+
+	# validates :content, length: {maximum: 140}
+	# validates :user, presence: true
+
+
+	# before_create
+	# after_create :send_email_to_user
+
+	# after_create :add_to_followers_newsfeed
 end
