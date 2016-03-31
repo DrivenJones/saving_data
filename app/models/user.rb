297c85d3df9be79d63113e89328733cf9628d55e
Veronica N. Chapman <vchapman@mycrowningjewel.com>
2016-03-31 16:49:34 -0400

    Commented an alternate way to validate a user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ActiveRecord::Base
 
   has_many :tweets 
   validates :type, inclusion: { in: %w(Designer RegularUser)}
+
+  # validate :type_is_either_designer_or_regular_user
+
+  # def type_is_either_designer_or_regular_user
+  # 	if self.type == "Designer" || self.type=="RegularUser"
+  # 		#do nothing
+  # 	else
+  # 		self.errors.add(:type, "You can only be a designer or regular user")
+  # 	end
+  # end
+  
 end
